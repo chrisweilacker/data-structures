6e4011aeb1f072df1641a874a3678570b1aa5176
Chris Weilacker <chrisweilacker@gmail.com>
2018-10-11 20:08:48 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,34 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (let i = 0; i < this.children.length; i ++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild is O(1);
+ * contains is O(n);
  */
