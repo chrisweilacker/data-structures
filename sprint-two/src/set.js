56c26ddb81ccb16719ad55f97c1671e59cb71d32
Chris Weilacker <chrisweilacker@gmail.com>
2018-10-11 20:15:15 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
+  this._storage.splice(this._storage.indexOf(item), 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add is O(1)
+ * contains is O(n)
+ * remove is O(n)
+ * 
  */
