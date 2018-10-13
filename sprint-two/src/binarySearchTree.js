478b37309dbe14c9281ff0e06dccd84bb50e29b6
Chris Weilacker <chrisweilacker@gmail.com>
2018-10-13 11:43:34 -0700

    Started binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,20 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
 };
 
+BinarySearchTree.prototype.insert = function (value) {
+
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (callBack) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
