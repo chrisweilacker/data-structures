756573604676c7fcbfbaea988f8c519fbec7cc67
Chris Weilacker <chrisweilacker@gmail.com>
2018-10-13 11:42:42 -0700

    Complete Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,16 +7,54 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  var keyPair = [k, v];
+  if (!this._storage.get(index)) {
+    // if the index doesn't exist
+    this._storage.set(index, [keyPair]);
+  } else {
+    // if the index does exist
+    // for each key/value pair in our array
+    var newKeyPair = this._storage.get(index);
+    for (let i = 0; i < newKeyPair.length; i ++) {
+      if (newKeyPair[i][0] === k) {
+        newKeyPair[i][1] = v;
+        this._storage.set(index, newKeyPair);
+        return;
+      }
+    }
+    //if value not found push into array and set
+    newKeyPair.push(keyPair);
+    this._storage.set(index, newKeyPair);
+  }
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
+  if (this._storage.get(index)) {
+    for (let i = 0; i < this._storage.get(index).length; i ++) {
+      if (this._storage.get(index)[i][0] === k) {
+        return this._storage.get(index)[i][1];
+      }
+    }
+  } else {
+    return undefined;
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) && this._storage.get(index).length > 1) {
+    var newKeyPair = this._storage.get(index);
+    for (let i = 0; i < newKeyPair.length; i ++) {
+      if (newKeyPair[i][0] === k) {
+        newKeyPair.splice(i, 1);
+        this._storage.set(index, newKeyPair);
+        return;
+      }
+    }
+  }
+  return this._storage.set(index, undefined);
 };
 
 
